98e6c5dcdbe85feecf45380d5b6691b7f77c9c43
Hex <lumengeared@gmail.com>
2022-06-27 10:17:39 +0800

    line feed

--- src/main.zig
+++ src/main.zig
@@ -18,6 +18,7 @@ pub fn main() anyerror!void {
     // where `Id` has a `describtion` and `value` method (`Param(Help)` is one such parameter).
     // The last argument contains options as to how `help` should print those parameters. Using
     // `.{}` means the default options.
+
     if (res.args.help)
         return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
 }
