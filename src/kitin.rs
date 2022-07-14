6a541edcda2a02411ed298b39c70b906453432dd
socean <lumengeared@gmail.com>
2022-07-14 21:56:01 +0800

    progress on project add subcommand

--- src/kitin.rs
+++ src/kitin.rs
@@ -1,14 +1,70 @@
 use serde::{Deserialize, Serialize};
 use std::{
+    fmt,
     io::{Read, Write},
-    path::Path,
+    str::FromStr,
 };
 
 const KITIN_PROJECT_FILE: &str = "kitin.yaml";
 
+#[derive(Deserialize, Debug)]
+pub enum KitinModuleSourceType {
+    Git,
+    Local,
+}
+
+impl FromStr for KitinModuleSourceType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Git" => Ok(KitinModuleSourceType::Git),
+            "Local" => Ok(KitinModuleSourceType::Local),
+            _ => Err(format!("Unknown source type: {}", s)),
+        }
+    }
+}
+
+impl fmt::Display for KitinModuleSourceType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            KitinModuleSourceType::Git => write!(f, "Git"),
+            KitinModuleSourceType::Local => write!(f, "Local"),
+        }
+    }
+}
+
+impl Serialize for KitinModuleSourceType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+impl Clone for KitinModuleSourceType {
+    fn clone(&self) -> Self {
+        match self {
+            KitinModuleSourceType::Git => KitinModuleSourceType::Git,
+            KitinModuleSourceType::Local => KitinModuleSourceType::Local,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
-struct KitinModule {
+pub struct KitinModule {
     source: Option<String>,
+    source_type: Option<KitinModuleSourceType>,
+}
+
+impl KitinModule {
+    pub fn new(source: String, source_type: KitinModuleSourceType) -> Self {
+        KitinModule {
+            source: Some(source),
+            source_type: Some(source_type),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -18,24 +74,27 @@ pub struct KitinProject {
 }
 
 impl KitinProject {
-    pub fn new() -> KitinProject {
+    pub fn new() -> Self {
         KitinProject {
             name: None,
-            modules: None,
+            modules: Some(std::collections::HashMap::new()),
         }
     }
 
-    // loads a .kit file from the current working directory
+    // loads a kitin.yaml file from the current working directory
     pub fn load_from_file(&mut self) {
         let path = std::path::Path::new(KITIN_PROJECT_FILE);
-        if path.exists() {
-            let mut file = std::fs::File::open(path).unwrap();
-            let mut contents = String::new();
-            file.read_to_string(&mut contents).unwrap();
-            let data: KitinProject = serde_yaml::from_str(&contents).unwrap();
-            self.name = data.name;
-            self.modules = data.modules;
+        if !path.exists() {
+            return;
         }
+        let mut file = std::fs::File::open(path).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let data: KitinProject = serde_yaml::from_str(&contents).unwrap();
+        self.name = data.name;
+        self.modules = data
+            .modules
+            .or_else(|| Some(std::collections::HashMap::new()));
     }
 
     // checks if a directory has a .kit file, and subsequently a kit project.
@@ -53,5 +112,9 @@ impl KitinProject {
 
     pub fn set_name(&mut self, name: &str) {
         self.name = Some(name.to_string());
+    }
+
+    pub fn add_module(&mut self, path: String, module: KitinModule) {
+        self.modules.as_mut().unwrap().insert(path, module);
     }
 }
--- src/project.rs
+++ src/project.rs
@@ -1,14 +1,15 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 
 use clap::{AppSettings, Arg, Command};
 
-use crate::kitin::{self, KitinProject};
+use crate::kitin::{self, KitinModule, KitinModuleSourceType, KitinProject};
 
 pub fn subcommand() -> Command<'static> {
     return Command::new("project")
         .about("Kitin project related commmands")
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(subcommand_init());
+        .subcommand(subcommand_init())
+        .subcommand(subcommand_add());
 }
 
 pub fn subcommand_init() -> Command<'static> {
@@ -17,18 +18,33 @@ pub fn subcommand_init() -> Command<'static> {
         .arg(Arg::with_name("name").required(false).index(1));
 }
 
+pub fn subcommand_add() -> Command<'static> {
+    return Command::new("add")
+        .about("Add a module to a project")
+        .arg(Arg::with_name("path").required(true).index(1))
+        .arg(Arg::with_name("source").required(true).index(2))
+        .arg(
+            Arg::with_name("source_type")
+                .default_value("Git")
+                .required(false)
+                .index(3),
+        );
+}
+
 pub fn run(matches: &clap::ArgMatches) {
     let mut project = kitin::KitinProject::new();
     let cwd = std::env::current_dir().unwrap();
 
     match matches.subcommand() {
         Some(("init", sub_matches)) => run_init(&mut project, &cwd, sub_matches),
+        Some(("add", sub_matches)) => run_add(&mut project, &cwd, sub_matches),
         _ => {}
     }
 }
 
 pub fn run_init(project: &mut KitinProject, cwd: &PathBuf, matches: &clap::ArgMatches) {
     if kitin::KitinProject::directory_has_project(cwd.as_path()) {
+        println!("Kitin project already exists in this directory");
         return;
     };
 
@@ -39,5 +55,23 @@ pub fn run_init(project: &mut KitinProject, cwd: &PathBuf, matches: &clap::ArgMa
         .unwrap();
     let name = matches.get_one::<String>("name").unwrap_or(&default_name);
     project.set_name(name);
+    project.save_to_file(cwd.as_path());
+}
+
+pub fn run_add(project: &mut KitinProject, cwd: &PathBuf, matches: &clap::ArgMatches) {
+    if !kitin::KitinProject::directory_has_project(cwd.as_path()) {
+        println!("No kitin project in current directory");
+        return;
+    };
+    project.load_from_file();
+
+    let path = matches.get_one::<String>("path").unwrap();
+    let source = matches.get_one::<String>("source").unwrap();
+    let source_type = matches.get_one::<String>("source_type").unwrap();
+    let module = KitinModule::new(
+        source.clone(),
+        KitinModuleSourceType::from_str(&source_type).unwrap(),
+    );
+    project.add_module(path.clone(), module);
     project.save_to_file(cwd.as_path());
 }
